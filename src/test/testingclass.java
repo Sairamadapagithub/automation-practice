dffd6dff0c16dc9984a8ba03be9dd1095887282c
Adapa janakiram <adapajanakiram1@gmail.com>
2026-06-20 20:02:58 +0530

    test

--- src/test/testingclass.java
+++ src/test/testingclass.java
@@ -4,5 +4,12 @@ public class testingclass {
 //tessting
 	public void test() {
 	System.out.println("output");
-}
+	System.out.println("output1");
+	System.out.println("output2");
+	System.out.println("output3");
+	System.out.println("output4");
+
+	
+	
+	}
 }
